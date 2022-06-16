481b38b5b2839ed37b71a3ddccf4f107012663eb
One user <stolypinskii@gmail.com>
2022-06-16 23:51:31 +0300

    test coverage all_book view

--- lib_app/tests/test_views.py
+++ lib_app/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User, Group, Permission
-from lib_app.models import Genre
+from lib_app.models import Genre, Author, Book
 
 
 class AllBookTestView(TestCase):
@@ -22,6 +22,27 @@ class AllBookTestView(TestCase):
         reader.groups.add(reader_group)
         manager.groups.add(manager_group)
 
+        genre_first = Genre.objects.create(genre_book='Русская классика', slug='russkaya_classika')
+        genre_second = Genre.objects.create(genre_book='Зарубежное', slug='zarybejnoe')
+
+        author_first = Author.objects.create(author_book='Достоевский Ф.М.', slug='dostoevskiy_fm')
+        author_second = Author.objects.create(author_book='Джек Лондон', slug='jack_london')
+
+        book_first = Book.objects.create(title='Преступление и наказание',
+                                         book_author=author_first,
+                                         book_genre=genre_first, book_amount=10,
+                                         slug='prestuplenie_i_nakazanie')
+
+        book_second = Book.objects.create(title='Бесы',
+                                          book_author=author_first,
+                                          book_genre=genre_first, book_amount=15,
+                                          slug='besy')
+
+        book_third = Book.objects.create(title='Белый клык',
+                                         book_author=author_second,
+                                         book_genre=genre_second, book_amount=20,
+                                         slug='beluy_kluk')
+
     def setUp(self):
         self.client = Client()
 
@@ -40,3 +61,9 @@ class AllBookTestView(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'lib_app/all_books.html')
+
+        search_response = self.client.get('/?search_select=title&search_query=Преступление и наказание')
+        self.assertEqual(search_response.status_code, 200)
+        self.assertEqual(search_response.templates[0].name, 'lib_app/all_books.html')
+        self.assertEqual(str(search_response.context['object_list'][0]), 'Преступление и наказание-Достоевский Ф.М.')
+        self.assertEqual(len(search_response.context['object_list']), 1)
